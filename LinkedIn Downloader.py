f30ce5989f9f0991fe9648852ce37b36be2c80a4
klayliu <dwliuliu@126.com>
2020-10-11 23:45:07 +0800

    fix regex pattern

--- LinkedIn Downloader.py
+++ LinkedIn Downloader.py
@@ -136,7 +136,7 @@ class Course(Browser):
         for chapter_section in chapter_sections:
             titles_mix.append(chapter_section.get_text().replace('\n', '').strip())  # get the chapter text
         for idx, title in enumerate(titles_mix):
-            re_pattern = re.compile(r'\d+m\s\d+s|\d+s|\d+m|\([VI].*?\)|\d+\squestions|\d\squestion|Chapter\sQuiz')
+            re_pattern = re.compile(r'\d+m\s\d+s|\d+s|\d+m|\([Vv]iewed.*?\)|\([Ii]n.*?[Pp]rogress\)|\d+\squestions|\d\squestion|Chapter\sQuiz')
             target_title = re.sub(re_pattern, '', title.strip()).split('   ')
             target_title = [s.strip() for s in target_title if s != '']
             target_title = [re.sub("[(),:?]", '', s).replace('/', '-').replace('  ',' ') for s in target_title if s != '']
